1ee35556c3d05e92c7ebd2966acb790799fc3e1a
Nathan <lutti99g4ming@gmail.com>
2024-08-04 15:11:36 +1200

    initial with pom

---
+++ src/main/java/nz/ac/massey/cs251/App.java
@@ -0,0 +1,13 @@
+package nz.ac.massey.cs251;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
